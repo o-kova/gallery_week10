88e59511eadf8ddfaab8bd56be550bc6d2378d46
o-kova <leo5.oak@gmail.com>
2023-08-15 15:57:36 +0300

    разделение на 2 страницы

---
+++ assets/scriptPage2.js
@@ -0,0 +1,9 @@
+let quantity = prompt("Какое количество единиц товара?");
+let price = prompt("Какая цена за единицу товара?");
+
+function calculateTotalPrice(quantity, price) {
+  // quantity - количество единиц товара
+  // price - цена за единицу товара
+  let quantity = prompt("Какое количество единиц товара?");
+  let price = prompt("Какая цена за единицу товара?");
+}
