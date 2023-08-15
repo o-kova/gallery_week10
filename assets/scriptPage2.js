a17e6524085c4e63d1eb0030d1963d86674f58aa
o-kova <leo5.oak@gmail.com>
2023-08-15 16:10:34 +0300

    changes

--- assets/scriptPage2.js
+++ assets/scriptPage2.js
@@ -1,9 +1,15 @@
-let quantity = prompt("Какое количество единиц товара?");
-let price = prompt("Какая цена за единицу товара?");
+// let quantity = prompt("Какое количество единиц товара?");
+// let price = prompt("Какая цена за единицу товара?");
 
-function calculateTotalPrice(quantity, price) {
+function calculateTotalPrice(quantity = 2, price = 15000000) {
   // quantity - количество единиц товара
   // price - цена за единицу товара
-  let quantity = prompt("Какое количество единиц товара?");
+  let quantity = prompt("Какое количество единиц товара?"); //prompt возвращает строку
   let price = prompt("Какая цена за единицу товара?");
+  quantity = Number(quantity);
+  price = Number(price);
+  totalСost = quantity * price;
+  //   totalСost = Number(quantity) * Number(price);
+  console.log(totalСost);
+  alert(totalCost.toLocaleString("ru-RU"));
 }
